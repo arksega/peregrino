c87243ac277b166056deba46a02cbbc4aedbcbc0
Sergio Gutierrez Alvarez <ark.sega@gmail.com>
2016-10-31 00:12:27 -0600

    Consider empty body as error on POST and PUT

--- hunterprice.py
+++ hunterprice.py
@@ -27,15 +27,14 @@ class JSONTranslator(object):
         # and allows you to read bytes from the request body.
         #
         if req.content_length in (None, 0):
-            return
+            if req.method in ('POST', 'PUT'):
+                raise falcon.HTTPBadRequest('Empty request body',
+                                            'A valid JSON is required.')
+            else:
+                return
         body = req.stream.read()
-        if not body:
-            raise falcon.HTTPBadRequest('Empty request body',
-                                        'A valid JSON document is required.')
-
         try:
             req.context['data'] = json.loads(body.decode('utf-8'))
-
         except (ValueError, UnicodeDecodeError):
             raise falcon.HTTPError(falcon.HTTP_753,
                                    'Malformed JSON',
